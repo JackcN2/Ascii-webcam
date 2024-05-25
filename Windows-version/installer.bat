7a5f0845af48574519dae04bdf1a616762ed35ee
JackcN2 <167788672+JackcN2@users.noreply.github.com>
2024-05-25 18:55:43 -0400

    Update installer.bat

--- Windows-version/installer.bat
+++ Windows-version/installer.bat
@@ -4,5 +4,6 @@ pip install -q  Pillow
 pip install -q opencv-python
 pip install -q SpeechRecognition
 pip install -q pyttsx3
+pip install -q requests
 @echo installer finished,  all packages sucessfull
 cmd/k
