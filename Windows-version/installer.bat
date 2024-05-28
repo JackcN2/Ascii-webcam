3b457ff628aaa3154ad3b560f9ba7758f8eb20d2
JackcN2 <167788672+JackcN2@users.noreply.github.com>
2024-05-28 17:44:32 -0400

    Add files via upload

--- Windows-version/installer.bat
+++ Windows-version/installer.bat
@@ -1,9 +1,21 @@
 @echo off
+@echo installing .
 pip install -q numpy
+Cls
+@echo installing ..
 pip install -q  Pillow
+Cls
+@echo installing ...
 pip install -q opencv-python
+Cls
+@echo installing .
 pip install -q SpeechRecognition
+Cls
+@echo installing ..
 pip install -q pyttsx3
-pip install -q requests
+Cls
+@echo installing ...
+pip install -q pyaudio
+Cls
 @echo installer finished,  all packages sucessfull
 cmd/k
